e7e5caefcc0643b0f54584aac662c11b7227f833
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2023-12-20 16:59:54 +0530

    environment is created, libraries are installed, setup.py is created

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from pathlib import Path
+import logging
+import setuptools
+
+with open('README.md', 'r', encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "ERP_Sales_Forecasting"
+AUTHOR_USER_NAME = "Priyanshu9898"
+SRC_REPO = "ERPsalesForecasting"
+AUTHOR_EMAIL = "priyanshumalaviya9210@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ERP sales Forecasting",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
